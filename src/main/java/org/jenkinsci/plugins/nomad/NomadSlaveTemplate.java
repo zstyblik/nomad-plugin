775940ae2c0744c40b71e046025734a4f9a3ebf8
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2017-10-04 14:59:57 +0200

    Add a flag whether to reuse containers
    Since containers are easy and cheap to start and destroy, I find it more
    convenient to be certain any job will start in a fresh container. Any
    residue from a previous job (might be from a different team) should be
    gone completely.
    
    For this, it is not sufficient to change "Idle termination time" to `0`,
    since queued jobs will still be executed on used containers.
    
    Instead, I added a flag "Reusable", which will in effect suspend a
    "computer" as soon as a job is scheduled on it. This means the computer
    will remain idle after the job is finished, and therefore the "Idle
    termination time" (still `0` obviously) will take effect. In practice
    every job will now run on a fresh container.

--- src/main/java/org/jenkinsci/plugins/nomad/NomadSlaveTemplate.java
+++ src/main/java/org/jenkinsci/plugins/nomad/NomadSlaveTemplate.java
@@ -27,6 +27,7 @@ public class NomadSlaveTemplate implements Describable<NomadSlaveTemplate> {
     private static final Logger LOGGER = Logger.getLogger(NomadSlaveTemplate.class.getName());
 
     private final int idleTerminationInMinutes;
+    private final Boolean reusable;
     private final int numExecutors;
 
     private final int cpu;
@@ -62,6 +63,7 @@ public class NomadSlaveTemplate implements Describable<NomadSlaveTemplate> {
             List<? extends NomadConstraintTemplate> constraints,
             String remoteFs,
             String idleTerminationInMinutes,
+            Boolean reusable,
             String numExecutors,
             Node.Mode mode,
             String region,
@@ -82,6 +84,7 @@ public class NomadSlaveTemplate implements Describable<NomadSlaveTemplate> {
         this.disk = Integer.parseInt(disk);
         this.priority = Integer.parseInt(priority);
         this.idleTerminationInMinutes = Integer.parseInt(idleTerminationInMinutes);
+        this.reusable = reusable;
         this.numExecutors = Integer.parseInt(numExecutors);
         this.mode = mode;
         this.remoteFs = remoteFs;
@@ -166,6 +169,10 @@ public class NomadSlaveTemplate implements Describable<NomadSlaveTemplate> {
 
     public int getIdleTerminationInMinutes() {
         return idleTerminationInMinutes;
+    }
+
+    public Boolean getReusable() {
+        return reusable;
     }
 
     public String getRegion() {
